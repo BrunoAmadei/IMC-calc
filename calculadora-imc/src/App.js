608c0dc43604be6d1d28a010806813eda566f5c3
BrunoAmadei <brunoamadei98@gmail.com>
2023-02-09 08:44:38 -0300

    tabela criada

--- calculadora-imc/src/App.js
+++ calculadora-imc/src/App.js
@@ -1,9 +1,12 @@
 import React, {useState} from 'react';
+import './App.css';
+import Tabela from './components/Tabela';
+
 
 function App() {
   return (
     <div>
-
+      <Tabela/>
     </div>
   );
 }
